f64a24fac8fc057ccc8b237cc3dc1a42dda1c0d1
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2022-12-01 22:28:05 +0100

    Day 1 Clojure

---
+++ clojure/day01.clj
@@ -0,0 +1,23 @@
+(ns day01
+  (:require [clojure.string :as str]))
+
+(defn sum-maxima [top coll]
+  (let [sorted (sort coll)]
+    (reduce + (take-last top sorted))))
+
+(defn get-sums []
+  (let [input (slurp "inputs/day01.txt")]
+    (->> (str/split input #"\n\n")
+         (map #(str/split % #"\n"))
+         (map #(map read-string %))
+         (map #(reduce + %)))))
+
+(defn part1 []
+  (sum-maxima 1 (get-sums)))
+
+(defn part2 []
+  (sum-maxima 3 (get-sums)))
+
+(do
+  (prn (part1))
+  (prn (part2)))
