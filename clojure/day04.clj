8f35b0a3f833231507c4a09e653dc846e94d8cfa
Guillaume Fayard <guillaume.fayard@pycolore.fr>
2022-12-04 11:19:34 +0100

    Day 4 Clojure

---
+++ clojure/day04.clj
@@ -0,0 +1,40 @@
+(ns day04 
+  (:require [clojure.set :as set]
+            [clojure.string :as str]))
+
+(defn assignment->set [assignment]
+  (let [[start end]
+        (as-> assignment it
+          (str/split it #"-")
+          (map read-string it))]
+    (-> (range start (inc end)) set)))
+
+(defn fully-overlap? [assign-pair]
+  (->> assign-pair
+       (filter #(= % (apply set/intersection assign-pair)))
+       (seq)))
+
+(defn partially-overlap? [assign-pair]
+  (->> assign-pair
+       (apply set/intersection)
+       (seq)))
+
+
+(defn get-assignments []
+  (->> (slurp "inputs/day04.txt")
+       (str/split-lines)
+       (map #(str/split % #","))
+       (map #(map assignment->set %))))
+
+(defn part1 []
+  (->> (get-assignments)
+       (filter fully-overlap?)
+       (count)))
+
+(defn part2 []
+  (->> (get-assignments)
+       (filter partially-overlap?)
+       (count)))
+
+(do (prn (part1))
+    (prn (part2)))
